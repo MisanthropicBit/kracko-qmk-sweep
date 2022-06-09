8405f8820320d3e1b74b401e7c202e39cc276085
aab <aab@connectedcars.dk>
2022-06-09 21:15:13 +0200

    Progress on the keymap

--- keymap.c
+++ keymap.c
@@ -7,17 +7,18 @@
 #include "keymap_danish.h"
 
 #define BASE_LAYER           0
-#define SYMBOL_LAYER         1
-#define CTRL_NAV_MEDIA_LAYER 2
-#define QMK_LAYER            3
-#define GAMING_LAYER         4
+#define SYMBOLS_LAYER        1
+#define EXTRA_SYMBOLS_LAYER  2
+#define CTRL_NAV_MEDIA_LAYER 3
+#define QMK_LAYER            4
+#define GAMING_LAYER         5
 
 #define KC_BRIGHT_UP         KC_PRINT_SCREEN
 #define KC_BRIGHT_DOWN       KC_SCROLL_LOCK
 #define KC_LPAREN            LSFT(KC_8)
 #define KC_RPAREN            LSFT(KC_9)
 #define KC_LBRACKET          LALT(KC_8)
-#define KC_RBRACKET          LALT(KC_8)
+#define KC_RBRACKET          LALT(KC_9)
 #define KC_SLBRACKET         LSFT(LALT(KC_8))
 #define KC_SRBRACKET         LSFT(LALT(KC_9))
 #define KC_DQUOTE            LSFT(KC_QUOTE)
@@ -29,94 +30,111 @@
 #define KC_LEND              LGUI(KC_LEFT)
 #define KC_REND              RGUI(KC_RIGHT)
 #define KC_SNAPSHOT_AREA     LSFT(LGUI(KC_4))
-#define KC_STAB              LSFT(KC_TAB)
-
-enum {
-    TD_CTRL_LAYERS,
-};
+#define KC_SFTTAB            LSFT(KC_TAB)
+#define KC_AMPERSAND         LSFT(KC_7)
+#define KC_BAR               LALT(KC_I)
 
 /***************************************************************************************************
  * QWERTY layer
 ****************************************************************************************************/
 const uint16_t BASE_QWERTY_LAYER[] = {
-    KC_Q,  KC_W,    KC_E,    KC_R,  KC_T,              KC_Y,   KC_U,    KC_I,     KC_O,   KC_P,\
-    KC_A,  KC_S,    KC_D,    KC_F,  LATLT_T(KC_G),     KC_H,   KC_J,    KC_K,     KC_L,   KC_SCLN,\
-    KC_Z,  KC_X,    KC_C,    KC_V,  KC_B,              KC_N,   KC_M,    KC_COMMA, KC_DOT, KC_SLSH,\
+    KC_Q,         KC_W, KC_E, KC_R,               KC_T,                  KC_Y,    KC_U,    KC_I,     KC_O,   KC_P,\
+    KC_A,         KC_S, KC_D, KC_F,               LATLT_T(KC_G),         KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN,\
+    LSFT_T(KC_Z), KC_X, KC_C, KC_V,               KC_B,                  KC_N,    KC_M,    KC_COMMA, KC_DOT, LSFT_T(KC_SLASH),\
 \
-        TD(TD_CTRL_LAYERS), KC_SPC,              KC_BSPC, MO(SYMBOLS_LAYER)\
+                              MO(CTRL_NAV_LAYER), KC_SPC,                KC_BSPC, MO(SYMBOLS_LAYER)
 };
 
 /***************************************************************************************************
  * Colemak DH layer
 ****************************************************************************************************/
 const uint16_t BASE_COLEMAK_DH_LAYER[] = {
-    KC_Q,  KC_W,    KC_F,    KC_P,  KC_B,              KC_J,   KC_L,    KC_U,     KC_Y,   KC_P,\
-    KC_A,  KC_R,    KC_S,    KC_T,  LALT_T(KC_G),      KC_M,   KC_N,    KC_E,     KC_I,   KC_O,\
-    KC_Z,  KC_X,    KC_C,    KC_D,  KC_V,              KC_K,   KC_H,    KC_COMMA, KC_DOT, KC_SLSH,\
+    KC_Q,         KC_W, KC_F, KC_P,               KC_B,                  KC_J,    KC_L,              KC_U,     KC_Y,   KC_P,\
+    KC_A,         KC_R, KC_S, KC_T,               LALT_T(KC_G),          KC_M,    KC_N,              KC_E,     KC_I,   KC_O,\
+    LSFT_T(KC_Z), KC_X, KC_C, KC_D,               KC_V,                  KC_K,    KC_H,              KC_COMMA, KC_DOT, LSFT_T(KC_SLSH),\
 \
-        TD(TD_CTRL_LAYERS), KC_SPC,              KC_BSPC, MO(SYMBOLS_LAYER)\
+                              MO(CTRL_NAV_LAYER), KC_SPC,                KC_BSPC, MO(SYMBOLS_LAYER)
 };
+
+#ifdef _USE_QWERTY_BASE_LAYER
+    #define _BASE_LAYER BASE_QWERTY_LAYER
+#else
+    #define _BASE_LAYER BASE_COLEMAK_DH_LAYER
+#endif
 
 /***************************************************************************************************
  * Symbols layer
 ****************************************************************************************************/
-const uint16_t SYMBOLS_LAYER[] = {
-    KC_1,       KC_2,      KC_3,        KC_4,         KC_5,               KC_6,     KC_7,      KC_8,     KC_9,        KC_0,
-    KC_PERCENT, KC_LPAREN, KC_LBRACKET, KC_SLBRACKET, KC_COMMA,           KC_EQUAL, KC_QUESTION, KC_QUOTE,  KC_PLUS,  KC_ASTERISK,
-    KC_DOLLAR,  KC_RPAREN, KC_RBRACKET, KC_SRBRACKET, KC_DOT,             KC_AT,    KC_EXCLAIM,  KC_DQUOTE, KC_MINUS, KC_SLASH,
+const uint16_t _SYMBOLS_LAYER[] = {
+    KC_1,       KC_2,      KC_3,                    KC_4,                    KC_5,         KC_6,     KC_7,        KC_8,      KC_9,     KC_0,\
+    KC_PERCENT, KC_LPAREN, KC_LBRACKET,             KC_SLBRACKET,            KC_AMPERSAND, KC_EQUAL, KC_QUESTION, KC_QUOTE,  KC_PLUS,  KC_ASTERISK,\
+    KC_DOLLAR,  KC_RPAREN, KC_RBRACKET,             KC_SRBRACKET,            KC_BAR,       KC_GRAVE, KC_EXCLAIM,  KC_DQUOTE, KC_MINUS, KC_AT,\
+\
+                           MO(EXTRA_SYMBOLS_LAYER), KC_SPACE,                ______,       _______
+};
 
-                                        KC_, KC_SPACE,               KC_, _______
+/***************************************************************************************************
+ * Extra symbols layer
+****************************************************************************************************/
+const uint16_t _EXTRA_SYMBOLS_LAYER[] = {
+    ______, ______, ______, ______, ______,             ______, ______,       ______, ______, ______,\
+    ______, ______, ______, ______, ______,             ______, KC_COLON,     ______, ______, ______,\
+    ______, ______, ______, ______, ______,             ______, KC_SEMICOLON, ______, ______, KC_BACKSLASH,\
+\
+                            ______, KC_SPACE,           ______, _______
 };
 
 /***************************************************************************************************
  * Control/Navigation layer
 ****************************************************************************************************/
-const uint16_t CTRL_NAV_LAYER[] = {
-    KC_, KC_,     KC_UP,   KC_,      KC_,           KC_BRIGHT_DOWN, KC_BRIGHT_UP,      KC_,      KC_,    KC_BOOTLOADER,
-    KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_,       KC_LEND, KC_LWORD, KC_RWORD, KC_REND,    KC_TAB,
-    KC_SNAPSHOT_AREA, KC_,     KC_,     KC_,        KC_, KC_COPY,  KC_PASTE, KC_CUT, KC_STAB,
-                                _______, KC_,           KC_, KC_                
+const uint16_t _CTRL_NAV_LAYER[] = {
+    _______,          _______, KC_UP,   _______,  _______,          KC_BRIGHT_DOWN, KC_BRIGHT_UP, _______,  _______,   KC_BOOTLOADER,\
+    KC_CAPS,          KC_LEFT, KC_DOWN, KC_RIGHT, _______,          KC_LEND,        KC_LWORD,     KC_RWORD, KC_REND,   KC_TAB,\
+    KC_SNAPSHOT_AREA, _______, _______, _______,  _______,          KC_COPY,        KC_PASTE,     KC_CUT,   KC_SFTTAB, ______,\
+\
+                                        _______,  KC_SPACE,         ______,         MO(CTRL_MEDIA_LAYER)
 };
 
 /***************************************************************************************************
  * Control/Media layer
 ****************************************************************************************************/
-const uint16_t CTRL_MEDIA_LAYER[] = {
-    KC_, KC_, KC_,            KC_,          KC_,            KC_, KC_, KC_, KC_, KC_,
-    KC_, KC_, KC_BRIGHT_DOWN, KC_BRIGHT_UP, KC_,            KC_, KC_MEDIA_PLAY_PAUSE, KC_AUDIO_MUTE, KC_, KC_,
-    KC_, KC_, KC_,            KC_,          KC_,            KC_, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_,
-                                _______, KC_, KC_,            KC_
+const uint16_t _CTRL_MEDIA_LAYER[] = {
+    _______, _______, ______, ______, ______,            _______, _______,        _______,           _______,             KC_BOOTLOADER
+    _______, _______, ______, ______, ______,            _______, KC_BRIGHT_UP,   KC_AUDIO_VOL_UP,   KC_MEDIA_PLAY_PAUSE, DF(BASE_QWERTY_LAYER)
+    _______, _______, ______, ______, ______,            _______, KC_BRIGHT_DOWN, KC_AUDIO_VOL_DOWN, KC_AUDIO_MUTE,       DF(BASE_COLEMAK_DH_LAYER)
+                              ______, ______,            _______, _______
 }
 
 /***************************************************************************************************
  * QMK (bootloader/reset) layer
 ****************************************************************************************************/
-const uint16_t QMK_LAYER[] = {
-    KC_, KC_, KC_, KC_, KC_,               KC_, KC_, KC_, KC_, KC_,
-    KC_, KC_, KC_, KC_, KC_,               KC_, KC_, KC_, KC_, KC_,
-    KC_, KC_, KC_, KC_, KC_,               KC_, KC_, KC_, KC_, KC_,
-                   KC_, KC_,               KC_, KC_                
+const uint16_t _QMK_LAYER[] = {
+    _______, _______, _______, _______, _______,               _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______,               _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______,               _______, _______, _______, _______, _______
+
+                               _______, _______,               _______, _______
 };
 
 /***************************************************************************************************
  * Gaming layer
 ****************************************************************************************************/
-const uint16_t GAMING_LAYER[] = {
-    KC_, KC_, KC_, KC_, KC_,               KC_, KC_, KC_, KC_, KC_,
-    KC_, KC_, KC_, KC_, KC_,               KC_, KC_, KC_, KC_, KC_,
-    KC_, KC_, KC_, KC_, KC_,               KC_, KC_, KC_, KC_, KC_,
-                   KC_, KC_,               KC_, KC_                
+const uint16_t _GAMING_LAYER[] = {
+    ______, ______,  KC_W,   ______, ______,               ______, ______, ______, ______, ______,
+    ______, KC_A,    KC_S,   KC_D,   ______,               ______, ______, ______, ______, ______,
+    KC_LCTL, ______, ______, ______, ______,               ______, ______, ______, ______, ______,
+                             ______, ______,               ______, ______                
 };
 
-const uint16_t QWERTY_LAYOUT[] = {
-    KC_Q,         KC_W,         KC_E,            KC_R,           KC_T,
-    KC_Y,         KC_U,         KC_I,            KC_O,           KC_P,
-    LSFT_T(KC_A), LT(5, KC_S),  LT(1, KC_D),     LT(3, KC_F),    KC_G,
-    KC_H,         LT(4, KC_J),  LT(2, KC_K),     LT(6,KC_L),     LSFT_T(KC_SCLN),
-    KC_Z,         LCTL_T(KC_X), LALT_T(KC_C),    KC_V,           KC_B,
-    KC_N,         KC_M,         LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
-    KC_P0,        KC_BSPC,      LT(7, KC_SPC),   KC_P1
-};
+/* const uint16_t QWERTY_LAYOUT[] = { */
+/*     KC_Q,         KC_W,         KC_E,            KC_R,           KC_T, */
+/*     KC_Y,         KC_U,         KC_I,            KC_O,           KC_P, */
+/*     LSFT_T(KC_A), LT(5, KC_S),  LT(1, KC_D),     LT(3, KC_F),    KC_G, */
+/*     KC_H,         LT(4, KC_J),  LT(2, KC_K),     LT(6,KC_L),     LSFT_T(KC_SCLN), */
+/*     KC_Z,         LCTL_T(KC_X), LALT_T(KC_C),    KC_V,           KC_B, */
+/*     KC_N,         KC_M,         LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH, */
+/*     KC_P0,        KC_BSPC,      LT(7, KC_SPC),   KC_P1 */
+/* }; */
 
 /***************************************************************************************************
  * Combos
@@ -150,27 +168,51 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_CTRL_LAYERS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cln_finished, dance_cln_reset)
 };
 
+typedef struct {
+    bool ctrl_nav_layer_pressed = false;
+    bool symbols_layer_pressed = false;
+} double_layer_state;
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // If both primary layer switches are pressed, activate a new layer
+        case MO(CTRL_NAV_LAYER):
+            if (record->event.pressed) {
+                if (double_layer_state.symbols_layer_pressed) {
+                    layer_on(CTRL_MEDIA_LAYER);
+                } else {
+                    double_layer_state.ctrl_nav_layer_pressed = true;
+                    layer_on(CTRL_NAV_LAYER);
+                }
+            } else {
+                double_layer_state.ctrl_nav_layer_pressed = false;
+            }
+            return false;
 
+        case MO(SYMBOLS_LAYER):
+            if (record->event.pressed) {
+                if (double_layer_state.ctrl_nav_layer_pressed) {
+                    layer_on(CTRL_MEDIA_LAYER);
+                } else {
+                    double_layer_state.symbols_layer_pressed = true;
+                    layer_on(SYMBOLS_LAYER);
+                }
+            } else {
+                double_layer_state.symbols_layer_pressed = false;
+            }
+            return false;
 
-
-
-
-
-
-
-
-
-
-
+        default:
+            return true;
+    }
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [BASE_LAYER] = LAYOUT_split_3x5_2(BASE_QWERTY_LAYOUT),
-    [1] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_WH_U, KC_BTN2, KC_TRNS, KC_TRNS, KC_BTN2, KC_NO, KC_BTN1, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [2] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LGUI, KC_NO, LCTL(KC_LALT), LCA(KC_LSFT), KC_TRNS, KC_HOME, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [3] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PIPE, KC_QUOT, KC_TRNS, KC_CIRC, KC_ASTR, KC_AMPR, KC_NO, KC_TRNS, KC_HASH, KC_TILD, KC_SLSH, KC_DQUO, KC_DLR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_BSLS, KC_GRV, KC_TRNS, RGB_RMOD, KC_TRNS, KC_TRNS, RGB_MOD),
-    [4] = LAYOUT_split_3x5_2(KC_TRNS, KC_COLN, KC_LT, KC_GT, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_AT, KC_TRNS, KC_NO, KC_EQL, KC_PLUS, KC_PERC, KC_TRNS, KC_EXLM, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_TRNS, KC_TRNS, KC_VOLU),
-    [5] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_TRNS, KC_NO, LCTL(KC_LALT), KC_TRNS, KC_TRNS, KC_TRNS, KC_F4, KC_F5, KC_F6, KC_F11, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [6] = LAYOUT_split_3x5_2(KC_PSLS, KC_7, KC_8, KC_9, KC_PPLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_1, KC_2, KC_3, KC_PMNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_PAST, KC_4, KC_5, KC_6, KC_PEQL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [7] = LAYOUT_split_3x5_2(BASE_COLEMAK_DH_LAYOUT)
+    [BASE_LAYER]          = LAYOUT_split_3x5_2(_BASE_LAYER),
+    [SYMBOLS_LAYER]       = LAYOUT_split_3x5_2(_SYMBOLS_LAYER),
+    [EXTRA_SYMBOLS_LAYER] = LAYOUT_split_3x5_2(_EXTRA_SYMBOLS_LAYER),
+    [CTRL_NAV_LAYER]      = LAYOUT_split_3x5_2(_CTRL_NAV_LAYER),
+    [CTRL_MEDIA_LAYER]    = LAYOUT_split_3x5_2(_CTRL_MEDIA_LAYER),
+    [QMK_LAYER]           = LAYOUT_split_3x5_2(_QMK_LAYER),
+    [GAMING_LAYER]        = LAYOUT_split_3x5_2(_GAMING_LAYER)
 };
